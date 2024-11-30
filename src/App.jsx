91ca9e381864d41d043f205e0193a44f661ad8f0
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-11-30 12:21:18 -0500

    Feat: Initial project config

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const App = () => {
+    return (
+        <main>
+            <h1 className="text-5xl text-orange-500 font-bold"> Welcome to our website</h1>
+        </main>
+    )
+}
+export default App;
