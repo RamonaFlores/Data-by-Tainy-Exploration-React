f2a496e94f1b2265dbf82df426e99c57b9534f17
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-11-30 20:02:50 -0500

    Feat: Hero Section Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,19 @@
-import React, {useRef, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import Button from "./Button.jsx";
 import {TiLocationArrow} from "react-icons/ti";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap"
+import {ScrollTrigger} from "gsap/all";
+
+//Allows animations to be fired by scrolling
+gsap.registerPlugin(ScrollTrigger)
+
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false)
     const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
-    const totalVideos= 3;
+    const totalVideos= 4;
     const nextVideoRef = useRef(null);
 
     const getVideoSrc =(index) => `videos/hero-${index}.mp4`
@@ -19,8 +24,13 @@ const Hero = () => {
     const handleMiniVdClick = () => {
         setHasClicked(true);
 
-        setCurrentIndex( (prevIndex) => prevIndex + 1);
+        setCurrentIndex( upcomingVideoIndex);
     }
+    useEffect(() => {
+        if(loadedVideos === totalVideos - 1){
+            setIsLoading(false);
+        }
+    },[loadedVideos])
     useGSAP( () => {if(hasClicked) {
 
         gsap.set('#next-video', {visibility: 'visible'})
@@ -40,8 +50,37 @@ const Hero = () => {
             ease:'power1.inOut',
         })
     }},{dependencies:  [currentIndex],revertOnUpdate: true})
+    useGSAP( () => {
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(14% 0%, 72% 0% ,88% 90%,0% 95%)',
+            borderRadius: '0% 0% 40% 10%'
+        })
+        //By setting the values of the polygon to 100% we're basically turning it into a square haha.
+
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%,100% 100%,0% 100%)',
+            borderRadius: '0% 0% 0% 0%',
+            ease:'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            }
+        })
+    })
   return (
+      // This is the loading screen
       <div className='relative h-dvh w-screen overflow-x-hidden'>
+          {isLoading && (
+              <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+                  <div className='three-body'>
+                      <div className='three-body__dot'/>
+                      <div className='three-body__dot'/>
+                      <div className='three-body__dot'/>
+                  </div>
+              </div>
+          )}
           {/* The super nice video is placed inside this div */}
           <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden
             rounded-lg bg-blue-75'>
